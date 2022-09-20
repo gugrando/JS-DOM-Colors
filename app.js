bc4e5c38ceeb57dea9c9f87affe6671c6a352059
Gustavo Grando Vieira <97984848+gugrando@users.noreply.github.com>
2022-09-20 14:41:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "blue", "yellow"];//Cores Simples
+const btn = document.getElementById("btn")//Botão que faz a troca
+const color = document.querySelector(".color")//Texto que identifica a Cor
+
+btn.addEventListener("click", function(){ //Adicionando o evento no botão
+    const randomNumber = getRandomNumber() //Numero aleatorio gerado por uma function
+
+    document.body.style.backgroundColor = colors[randomNumber]//Muda para o index do numero aleatorio de 0 a 3
+    color.textContent = colors[randomNumber]//Muda o texto de color para o index de colors 0 a 3
+})
+
+function getRandomNumber(){//função que gera p número aleatorio 
+    return Math.floor(Math.random()*colors.length)
+}
