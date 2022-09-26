fd15c532ad9f1660b702f14642c27f30eeeb845b
Gustavo Grando Vieira <97984848+gugrando@users.noreply.github.com>
2022-09-26 13:48:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ btn.addEventListener("click", function(){ //Adicionando o evento no botão
 
     document.body.style.backgroundColor = colors[randomNumber]//Muda para o index do numero aleatorio de 0 a 3
     color.textContent = colors[randomNumber]//Muda o texto de color para o index de colors 0 a 3
-    color.style.color = colors
+    color.style.color = colors[randomNumber]
 })
 
 function getRandomNumber(){//função que gera p número aleatorio 
